ae0b8281e3e2f9e4ec35c28a440840fa88736f8f
Ubuntu <mdsolimu@ualberta.ca>
2021-08-19 00:53:13 +0000

    Fixed: Specify default argument in the declaration not the friend declaration

---
+++ minisat_unmodified/core/depend.mk
@@ -0,0 +1,27 @@
+/home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.o /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.or /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.od /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.op: /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.cc \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Sort.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Vec.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/IntTypes.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/XAlloc.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Heap.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Alg.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/utils/Options.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/utils/ParseUtils.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/core/SolverTypes.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Map.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Alloc.h
+/home/ubuntu/cbmc-minisat/minisat_unmodified/core/Main.o /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Main.or /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Main.od /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Main.op: /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Main.cc \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/utils/System.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/IntTypes.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/utils/ParseUtils.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/utils/Options.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Vec.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/XAlloc.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Dimacs.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/core/SolverTypes.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Alg.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Map.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Alloc.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/core/Solver.h \
+ /home/ubuntu/cbmc-minisat/minisat_unmodified/mtl/Heap.h
