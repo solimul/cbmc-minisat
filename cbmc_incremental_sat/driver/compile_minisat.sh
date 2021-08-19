67a3a33d3cda8f63080f3b242150d128ca12a2aa
Ubuntu <mdsolimu@ualberta.ca>
2021-08-19 23:36:59 +0000

    ipasir_drive is now hooked with minisat220

--- cbmc_incremental_sat/driver/compile_minisat.sh
+++ cbmc_incremental_sat/driver/compile_minisat.sh
@@ -1,5 +1,6 @@
 #rm icnf_driver
 # cp /workspace/ipasir/sat/minisat220/libipasirminisat220.a .
-g++ -I. -o icnf_driver main.cpp -L. -lipasirminisat220
+#g++ -I. -o icnf_driver main.cpp -L. -lipasirminisat220
+g++ -I. -o icnf_driver main.cpp -L../../ipasir/sat/minisat220/ -lipasirminisat220
 #./icnf_driver ../cnfs/cbmc_cnf_2NCtXR 
 #./icnf_driver ../cnfs/cbmc_cnf_pG438A
